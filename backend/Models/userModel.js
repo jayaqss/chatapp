a315a2cce6af253a4e41a6fda7d6431609f8b2d8
Christopher Heron Dcosta <99186938+ChristopherQSS@users.noreply.github.com>
2022-02-08 02:15:45 +0530

    Add files via upload

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -15,8 +15,12 @@ const userSchema = mongoose.Schema(
   { timestamps: true }
 );
 
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 userSchema.pre('save', async function (next) {
-  if (!this.modified) {
+  if (!this.isModified) {
     next();
   }
 
