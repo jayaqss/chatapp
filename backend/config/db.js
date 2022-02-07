3189178ffb3bf8195f23ab6061153944bfda39e8
Christopher Heron Dcosta <99186938+ChristopherQSS@users.noreply.github.com>
2022-02-08 02:12:35 +0530

    Add files via upload

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(
+      'mongodb+srv://projectqss:B5lgSeR7prDwxAJ1@qsscluster.fthw9.mongodb.net/chatapp?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+  return jwt.sign({ id }, 'B5lgSeR7prDwxAJ1', {
     expiresIn: '30d',
   });
 };
