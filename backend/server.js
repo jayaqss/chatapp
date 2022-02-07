d4e46c9cb1b719f6c43bf52de08d8b5e3a145789
Christopher Heron Dcosta <99186938+ChristopherQSS@users.noreply.github.com>
2022-02-08 02:16:19 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const { chats } = require('./data/data');
 const connectDB = require('./config/db');
 const colors = require('colors');
 const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 dotenv.config();
 
@@ -27,5 +28,9 @@ app.get('/api/chat/:id', (req, res) => {
   res.send(singleChat);
 });
 
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(5000, console.log(`Server Started on PORT ${PORT}`.yellow.bold));
