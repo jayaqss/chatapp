9607e3f5c593f6472a8871e6195e2694a2d5bb5a
Christopher Heron Dcosta <99186938+ChristopherQSS@users.noreply.github.com>
2022-03-30 12:05:15 +0530

    Add files via upload

--- angular/src/app/services/authentication.service.ts
+++ angular/src/app/services/authentication.service.ts
@@ -1,1 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
 
+export interface LoginForm {
+  email: string;
+  password: string;
+}
+
+export interface User {
+  name?: string;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  constructor(private http: HttpClient) {}
+
+  login(loginForm: LoginForm) {
+    return this.http.post<any>('http://localhost:5000/api/user/login', {
+      email: loginForm.email,
+      password: loginForm.password,
+    });
+  }
+
+  signup(user: User) {
+    return this.http.post<any>('http://localhost:5000/api/user', {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      confirmPassword: user.confirmPassword,
+    });
+  }
+}
